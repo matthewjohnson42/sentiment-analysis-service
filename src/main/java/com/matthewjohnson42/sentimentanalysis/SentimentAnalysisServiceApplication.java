0f30c50ccc6021954366505cf12c171a70575e29
root <dev.shell.email@gmail.com>
2019-07-08 21:39:56 -0500

    making it into a springboot app

---
+++ src/main/java/com/matthewjohnson42/sentimentanalysis/SentimentAnalysisServiceApplication.java
@@ -0,0 +1,12 @@
+package com.matthewjohnson42.sentimentanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentAnalysisServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentimentAnalysisServiceApplication.class, args);
+    }
+}
